4fb2bd67455ba9691ad2dacb2a84396b5a829c9f
b <sylveons@mail.com>
2025-06-07 17:10:34 -0400

    Leetcode #217 - Contains Duplicate

---
+++ Easy/Duplicates.rs
@@ -0,0 +1,17 @@
+use std::collections::HashSet;
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        // Create a Set to keep track of all numbers in vec 
+        let mut set = HashSet::new();
+        // Iterate through the nums list and adds every number to set
+        for num in nums{
+            if // Duplicate found
+            set.contains(&num){
+                return true
+            } else {
+                set.insert(num);
+            }
+        }
+        false // Loop finishes, No duplicate
+    }
+}
