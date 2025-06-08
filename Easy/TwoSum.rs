94efdf12e266aafe8aeb89ad446539584119a3e0
b <sylveons@mail.com>
2025-06-07 20:57:55 -0400

    Leetcode #1 - Two Sum

---
+++ Easy/TwoSum.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        //initializes a new hashmap
+        let mut map = HashMap::new();
+        //For each number in list, get index and corresponding number
+        for (i, num) in nums.iter().enumerate() {
+        // If key found, return the current index, and the complements index
+        let complement = target - *num;
+            if map.contains_key(&complement) {
+            let j = map[&complement]; 
+            return vec![i as i32, j as i32] 
+            }
+            map.insert(*num, i);
+        }
+    //If no solution found
+    return vec![]
+   
+}
